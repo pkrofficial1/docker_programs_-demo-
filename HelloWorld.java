402c60e415a1627ed97fc5870d71465653aaf9b7
Praveen Kumar M <www.pkrofficial@gmail.com>
2024-08-12 12:30:00 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Praveen");
+        System.out.println("   _____  ");
+        System.out.println("  |     | ");
+        System.out.println("  |  ^ ^ |");
+        System.out.println("  |   -  |");
+        System.out.println("  | \\___/ |");
+        System.out.println("   -------");
     }
 }
